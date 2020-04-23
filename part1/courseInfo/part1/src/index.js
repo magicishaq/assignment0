a4034eb23ff09afcde583532d9d609c0bf38dad8
Ishaq Khan <ikhan@uk.nex.global>
2020-04-23 15:12:58 +0100

    1.1 course information step1 complete

--- part1/courseInfo/part1/src/index.js
+++ part1/courseInfo/part1/src/index.js
@@ -1,30 +1,48 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const Hello = (props) => {
+const Header = (props) => {
   return(
-<></>
+<>
+<h1> {props.course} </h1>
+</>
+  )
+}
+const Content = (props) => {
+  return (
+    <>
+<p>{props.part1} {props.exercise1}</p>
+  <p>{props.part2} {props.exercise2}</p>
+  <p>{props.part3} {props.exercise3} </p>
+
+  
+    </>
   )
 }
 
+const Total = (props) => {
+  const exerciseCount = props.exercise1 + props.exercise2 + props.exercise3
+  return (
+    <>
+    <p>{props.numString} {exerciseCount}</p>
+    </>
+  )
+}
 
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
-  const exercises1 = 10
+  const exercise1 = 10
   const part2 = 'Using props to pass data'
-  const exercises2 = 7
+  const exercise2 = 7
   const part3 = 'State of a component'
-  const exercises3 = 14
+  const exercise3 = 14
+  const numString = 'Number of exercises'
 return (
   <div>
-    <h1>{course}</h1>
-    <p>
-      {part1} {exercises1}
-    </p>
-    <p>{part2} {exercises2}</p>
-    <p>{part3} {exercises3}</p>
-    <p>Number of exercises {exercises1 + exercises2 + exercises3} </p>
+    <Header course={course}></Header>
+    <Content part1={part1} exercise1={exercise1} part2={part2} exercise2={exercise2} part3={part3} exercise3={exercise3}></Content>
+    <Total exercise1={exercise1} exercise2={exercise2} exercise3={exercise3} numString={numString}></Total>
   </div>
 )
 }
