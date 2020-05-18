1bd0fb0d52c689595d181f0e1cf6d4cae0214eee
Ishaq Khan <ikhan@uk.nex.global>
2020-05-18 18:09:12 +0100

    completed tutorial

--- part1/courseInfo/part1/src/index.js
+++ part1/courseInfo/part1/src/index.js
@@ -201,86 +201,174 @@
 
 //1d a more complex state
 //debugging react apps
-import React,{useState} from 'react'; 
+// import React,{useState} from 'react'; 
+// import ReactDOM from 'react-dom'; 
+
+
+// const App = () => 
+// {
+//   //setting two states in one object
+//   const [clicks, setClicks] = useState({
+//     left: 0, 
+//     right: 0,
+//     allClicks: []
+//   });
+
+
+//   //handles the clicking history
+  
+//   const History = (props)=> {
+//     const message = 'app is used by clicking buttons'
+// if(props.allClicks.length === 0){
+//   return (
+//     <div>
+//       {message}
+//     </div>
+//   )
+// }
+//     return(
+//       <div>
+//         button press history: {props.allClicks.join('')}
+//         </div>
+//     )
+//   }
+
+//   //component that handles the button
+
+//   const Button = ({onClick, text}) => {
+//     return(
+//       <div>
+//         <button onClick={onClick}>
+//           {text}
+//         </button>
+//       </div>
+//     )
+//   }
+
+//   //chanages the value of the left click but updating left and leaving right as is
+//   //using the spread syntax
+//   ///..clicks creates  new object that copies all the properties of the click object. by specifing the new property in newClicks, 
+//   //changes the property
+//   //it is forbidden to mutate state directly (clicks.right ++ ) has to be done by setting state to a new object
+
+//   const handleLeftClick = () => {
+//     const newClicks = {
+//       ...clicks, 
+//       left: clicks.left  + 1,
+//       allClicks: clicks.allClicks.concat('L')
+    
+//     }
+//     setClicks(newClicks); 
+//   }
+//   const handleRightClick = () => {
+//     const newClicks = {
+//       ...clicks, 
+//       right: clicks.right + 1, 
+//       allClicks: clicks.allClicks.concat('R')
+
+//     }
+//     setClicks(newClicks); 
+//   }
+
+//   return(
+//     <div>
+//       <div>
+//        {clicks.left}
+//        <Button onClick={handleLeftClick} text="left button" />
+//        {clicks.right}
+//        <Button onClick={handleRightClick} text="right button" /> 
+//       </div>
+//       <History allClicks={clicks.allClicks} /> 
+//       </div> 
+//   )
+// }
+
+// ReactDOM.render(<App/>,document.getElementById('root')); 
+
+
+//RUles of the Hook
+
+//hook based states can only be defined in an component
+
+import React, {useState} from 'react'; 
 import ReactDOM from 'react-dom'; 
 
+//Passing event handlers to child components 
+//turning button into separate component
+const Button = (props) => {
+  return(
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+  )
+}
 
-const App = () => 
-{
-  //setting two states in one object
-  const [clicks, setClicks] = useState({
-    left: 0, 
-    right: 0,
-    allClicks: []
-  });
+//Displaying the value 
+//never define components in other componnents
+const Display = (props) => {
+  return(
+    <h1><strong>The Value is: </strong>{props.value}</h1>
+  )
+}
 
+//Event handling revisited
 
-  //handles the clicking history
-  
-  const History = (props)=> {
-    const message = 'app is used by clicking buttons'
-if(props.allClicks.length === 0){
+const Thing = () =>{
+  const [value, setValue] = useState('Ishaq'); 
+  const changeValue = () => setValue('Khan')
+  //returning a function is used to put in parameters in to the function call
+  // const placeHtml = (string) => {
+  //   const changeTheDom = () => {
+  //     document.getElementById('khan').innerHTML = string; 
+  //   }
+  //   return changeTheDom; 
+  // }
+  //condesing the function
+  const placeHtml = (string) =>  () => {document.getElementById('khan').innerHTML = string}
+  //function to change state in an application
+  //returns a new function with a new value set
+  const setToValue = (newValue) => () => {setValue(newValue)}
+
   return (
     <div>
-      {message}
+    <Display value={value}></Display>
+    <button onClick={changeValue}> view second name </button> 
+    <button onClick={placeHtml('Boo')}>Boo</button>
+    <button onClick={placeHtml('Mario Brothers')}>Mario Brothers</button>
+    <button onClick={placeHtml('hello , react')}>Hello React</button>
+ 
+    <p id="khan"></p>
+    <p id='ishaq'></p>
+
+    <h2> These buttons will change the Value </h2>
+    <Display value={value}></Display>
+    <button onClick={setToValue(1000)}>1000</button>
+    <button onClick={setToValue(100)}>100</button>
+    <button onClick={setToValue(99)}>99</button>
+    <button onClick={setToValue('Crocodile')}>Animal</button>
+    <Button handleClick={setToValue('Doggy')} text={'Woof'}></Button>
+    <Button handleClick={setToValue('Rawr')} text={'Dino'}></Button>
     </div>
   )
 }
-    return(
-      <div>
-        button press history: {props.allClicks.join('')}
-        </div>
-    )
-  }
 
-  //component that handles the button
-
-  const Button = ({onClick, text}) => {
-    return(
-      <div>
-        <button onClick={onClick}>
-          {text}
-        </button>
-      </div>
-    )
-  }
-
-  //chanages the value of the left click but updating left and leaving right as is
-  //using the spread syntax
-  ///..clicks creates  new object that copies all the properties of the click object. by specifing the new property in newClicks, 
-  //changes the property
-  //it is forbidden to mutate state directly (clicks.right ++ ) has to be done by setting state to a new object
-
-  const handleLeftClick = () => {
-    const newClicks = {
-      ...clicks, 
-      left: clicks.left  + 1,
-      allClicks: clicks.allClicks.concat('L')
-    
-    }
-    setClicks(newClicks); 
-  }
-  const handleRightClick = () => {
-    const newClicks = {
-      ...clicks, 
-      right: clicks.right + 1, 
-      allClicks: clicks.allClicks.concat('R')
-
-    }
-    setClicks(newClicks); 
-  }
+const App = (props) => {
+  const [value, setValue] = useState(10);
+  const resetToZero = () => setValue(0) 
+  const addOne = () => setValue(value + 1); 
+  const handleClick = () => document.getElementById('ishaq').style.backgroundColor = 'green'; 
 
   return(
     <div>
-      <div>
-       {clicks.left}
-       <Button onClick={handleLeftClick} text="left button" />
-       {clicks.right}
-       <Button onClick={handleRightClick} text="right button" /> 
+      {value}
+      <button onClick={resetToZero} > reset to zero </button> 
+      <button onClick={addOne}> Adds one to counter </button>
+      <div id="ishaq" onClick={handleClick}>Testing if this is clickable</div> 
+      <Thing> </Thing>
+
       </div>
-      <History allClicks={clicks.allClicks} /> 
-      </div> 
   )
 }
 
-ReactDOM.render(<App/>,document.getElementById('root')); 
+ReactDOM.render(<App/>, document.getElementById('root')); 
+ 
