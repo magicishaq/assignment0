6665ba777921e907bb2e302f4da3a8c8aaff4634
Ishaq Khan <ikhan@uk.nex.global>
2020-05-19 20:18:47 +0100

    done

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,17 +1,100 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+/**
+ * Part 6 unicafe 
+ */
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Button = (props) => {
+  
+  return (
+<button onClick={props.handleClick}>
+  {props.text}
+</button>
+  )
+}
+
+const Stats = (props) => {
+  
+  return (
+      <table>
+        <tr>
+<td>Good</td>
+<td>{props.good}</td>
+        </tr>
+        <tr>
+<td>Bad</td>
+<td>{props.bad}</td>
+        </tr>
+        <tr>
+<td>Netrual</td>
+<td>{props.netural}</td>
+        </tr>
+        <tr>
+<td>All</td>
+<td>{props.all}</td>
+        </tr>
+        <tr>
+<td>Average</td>
+<td>{props.average}</td>
+        </tr>
+        <tr>
+<td>Positive</td>
+<td>{props.positive} % </td>
+        </tr>    
+        </table> 
+  )
+}
+ const App = () => {
+   const [good, setGood] = useState(0); 
+   const [netural, setNetural] = useState(0); 
+   const [bad, setBad] = useState(0); 
+   const [counter, setCounter] = useState(0);  
+   const [average, setAverage] = useState(0); 
+   const handleClick = (newValue) => () => {
+    setCounter(counter + 1); 
+     if(newValue === 'good'){   
+       setAverage(average + 1)  
+       return setGood(good + 1); 
+     }else if (newValue === 'bad'){
+       setAverage(average - 1); 
+      return setBad(bad + 1); 
+     }else{
+       setAverage(average)
+      return setNetural(netural + 1 )
+      
+     }
+   }
+
+   const getAverage = () => {
+    return good / counter
+  }
+
+   const positive = () => {
+     return getAverage() * 100;  
+   }
+   if(counter > 0) {
+   return(
+     <div>
+     <h1>Give Feedback</h1>
+<Button text="good" handleClick={handleClick('good')}></Button>
+<Button text="netural" handleClick={handleClick('netural')}></Button>
+<Button text="bad" handleClick={handleClick('bad')}> </Button>
+<Stats  good={good} netural={netural} bad={bad} all={counter} average={getAverage()} positive={positive()}></Stats>
+</div>
+
+   )
+   }else {
+     return (
+    <div>
+    <h1>Give Feedback</h1>
+<Button text="good" handleClick={handleClick('good')}></Button>
+<Button text="netural" handleClick={handleClick('netural')}></Button>
+<Button text="bad" handleClick={handleClick('bad')}> </Button>
+<p> No feedback given </p>
+</div>
+     )
+   }
+ }
+
+ ReactDOM.render(<App/>, document.getElementById('root')); 
