f143e834a815f080e40af8ce1c43cb8ab6df811f
Ishaq Khan <ikhan@uk.nex.global>
2020-05-29 15:02:54 +0100

    done part 2.1

--- part1/courseInfo/part1/src/index.js
+++ part1/courseInfo/part1/src/index.js
@@ -1,39 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Header from './component/header'
+import Content from './component/content' //part compent is defined here
+import Total from './component/total'
 
-const Header = (props) => {
-  return(
-<>
-<h1> {props.course} </h1>
-</>
-  )
-}
-
-
-const Part = (props) => {
+const Course = ({course}) => {
   return (
-<>
-  <p>{props.part} {props.exercise}</p>
-</>
+        <div>
+          <Header course={course.name}></Header>
+          <Content part1={course.part[0].name} exercise1={course.part[0].exercises} part2={course.part[1].name} exercise2={course.part[1].exercises} part3={course.part[2].name} exercise3={course.part[2].exercises}></Content>
+          <Total exercise1={course.part[0].exercises} exercise2={course.part[1].exercises} exercise3={course.part[2].exercises} numString={course.numString}></Total>
+        </div>
   )
-}
-const Content = (props) => {
-  return (
-    <div>
-      <Part part={props.part1} exercise={props.exercise1}></Part>
-      <Part part={props.part2} exercise={props.exercise2}></Part>
-      <Part part={props.part3} exercise={props.exercise3}></Part>
-    </div>
-  )
-}
 
-const Total = (props) => {
-  const exerciseCount = props.exercise1 + props.exercise2 + props.exercise3
-  return (
-    <>
-    <p>{props.numString} {exerciseCount}</p>
-    </>
-  )
 }
 
 const App = () => {
@@ -57,11 +36,7 @@ const App = () => {
   }
   
 return (
-  <div>
-    <Header course={course.name}></Header>
-    <Content part1={course.part[0].name} exercise1={course.part[0].exercises} part2={course.part[1].name} exercise2={course.part[1].exercises} part3={course.part[2].name} exercise3={course.part[2].exercises}></Content>
-    <Total exercise1={course.part[0].exercises} exercise2={course.part[1].exercises} exercise3={course.part[2].exercises} numString={course.numString}></Total>
-  </div>
+<Course course={course} /> 
 )
 }
 
