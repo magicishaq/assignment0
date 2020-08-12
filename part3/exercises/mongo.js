886a63dd38c7afe74c888c009e8861dc396523c6
Ishaq Khan <ikhan@uk.nex.global>
2020-08-12 16:26:19 +0100

    finised the second task

--- part3/exercises/mongo.js
+++ part3/exercises/mongo.js
@@ -2,9 +2,10 @@ const mongoose = require('mongoose')
 const pasword = 'Password123'
 const dbName = 'phonebook-app'
 
-if(process.argv.length < 3) {
+if(process.argv.length < 2) {
     console.log('Please provide the password as an argument : node mongo.js <password>')
 }
+
 
 const password = process.argv[2]
 const theName = process.argv[3]
@@ -20,17 +21,25 @@ const phoneSchema = new mongoose.Schema({
 })
 
 const Phone = mongoose.model('Phone', phoneSchema)
-
 const phone = new Phone({
     name: theName,
     phone: phoneNumber
 })
 
+
+Phone.find({}).then(result => {
+result.forEach(entry => {
+    console.log(entry)
+    })
+    mongoose.connection.close()
+})
+
+if(theName !== undefined && phoneNumber !== undefined) {
 phone.save().then(result => {
     console.log(`added ${theName} number ${phoneNumber} to phonebook`); 
     mongoose.connection.close()
 
 })
-
+}
 
 
