0cdabf7700f70675d65ec5841be2edb26d3d08bf
Ishaq Khan <ikhan@uk.nex.global>
2020-08-13 13:55:29 +0100

    added cors

--- part3/exercises/index.js
+++ part3/exercises/index.js
@@ -17,6 +17,7 @@ const phone =[
 
   const express = require('express'); 
   const app = express()
+  
 
   app.get('/', (request, response) => {
       response.send('<h1>Welcome to the phonebook </h1>')
